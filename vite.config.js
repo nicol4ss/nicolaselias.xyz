a0ff31073e1ef1a6f67307f0244316b5b5ceac77
Nicolas Elias <nicolaselias@pm.me>
2024-11-18 13:58:03 -0300

    add: build configs

--- vite.config.js
+++ vite.config.js
@@ -2,12 +2,7 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import glsl from "vite-plugin-glsl";
 
-// https://vite.dev/config/
 export default defineConfig({
   base: "/",
   plugins: [react(), glsl()],
-  esbuild: {
-    loader: "jsx",
-    include: /src\/.*\.js$/, // aplica o loader apenas aos arquivos .js dentro do diretório src
-  },
 });
